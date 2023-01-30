f76c259a931d756274a7aedb4d1484d800ee69be
Evgen J <68916909+EvgenJY2K2@users.noreply.github.com>
2023-01-30 20:29:18 +0200

    Delete Week 13 directory

--- Week 13/Task 3.py
+++
@@ -1,7 +0,0 @@
-rows = int(input("Enter number of rows:\n"))
-for x in range(0, rows):
-    for j in range(rows-x-1):
-        print("  ", end="")
-    for k in range(2*x+1):
-        print("* ", end = "")
-    print()
